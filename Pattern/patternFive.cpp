93289a70dc68dcea22a394d01000e48680862a22
CodeCommandBD <shantokumar22@gmail.com>
2025-06-11 18:57:48 +0600

    update

--- Pattern/patternFive.cpp
+++ Pattern/patternFive.cpp
@@ -6,10 +6,10 @@ int main(){
     int n, row, col;
     cin>> n;
 
-    for(row = 1; row<=n; row++){
+    for(row = n; row >=1; row--){
         for(col =1; col<=row; col++){
             
-                cout << char(col + 64); 
+                cout << row; 
             
         }
         cout<< endl;
