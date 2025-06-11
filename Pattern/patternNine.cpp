d517ffb0bd4c9dc9031dd5821d6c48e49a374dd7
CodeCommandBD <shantokumar22@gmail.com>
2025-06-12 00:18:31 +0600

    update

--- Pattern/patternNine.cpp
+++ Pattern/patternNine.cpp
@@ -6,7 +6,18 @@ int main(){
     int n, row, col;
     cin>> n;
 
-    for(row = n; row >= 1; row--){
+
+    for(row=1; row<=n; row++){
+        for(col=1; col<= n-row; col++){
+            cout << " ";
+        }
+        for(col=1; col<=row; col++){
+            cout<< col;
+        }
+        cout<<endl;
+    }
+
+    for(row = n-1; row >= 1; row--){
         for(col = 1; col<= n - row; col++){
                 cout << " "; 
         }
