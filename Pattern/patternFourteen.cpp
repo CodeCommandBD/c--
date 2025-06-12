df7d8983ac517582059c5bd439522ecf9cf9b8f8
CodeCommandBD <shantokumar22@gmail.com>
2025-06-12 13:23:22 +0600

    update

--- Pattern/patternFourteen.cpp
+++ Pattern/patternFourteen.cpp
@@ -6,7 +6,18 @@ int main(){
     int n, row, col;
     cin>> n;
 
-    for(row = n; row >= 1; row--){
+
+     for(row = 1; row <= n; row++){
+        for(col =1; col<= n-row; col++){
+            cout<< " ";
+        }
+        for(col =1; col<= 2*row-1; col++){
+            cout<< "*";
+        } 
+        cout<< endl;
+    }
+
+    for(row = n - 1; row >= 1; row--){
         for(col =1; col<= n-row; col++){
             cout<< " ";
         }
